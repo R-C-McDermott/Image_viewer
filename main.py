91e3412f82f2f03df0fb0677cc4fa28c2f84fb96
R-C-McDermott <ryan.c.mcdermott4@gmail.com>
2021-08-11 17:24:13 +0100

    Addition of main script (main.py)

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+Image editor programme - by Ryan C. McDermott
+
+Written using Python 3.6.1
+
+Packages used:
+- NumPy
+- Scikit-Image
+- Pillow
+- Open-CV
+
+'''
+
+# def main():
+
+
+if __name__ == '__main__':
+    main()
